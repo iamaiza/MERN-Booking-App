fd2e58fe191b9003ca369617014454c65e5cffa5
iamaiza <aiza9382@gmail.com>
2023-05-09 09:27:53 +0500

    header commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LogoIcon, SearchIcon, HambargerIcon, UserIcon } from "./icons/Icons";
+
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between mb-5">
+            <a href="" className="flex items-center gap-1 text-primary">
+                <LogoIcon />
+                <span className="font-bold text-xl">airbnb</span>
+            </a>
+            <div className="flex gap-3 border rounded-full py-3 px-7 shadow-md shadow-gray-300">
+                <div className="font-bold">Anywhere</div>
+                <div className="border-l"></div>
+                <div className="font-bold">Anyweek</div>
+                <div className="border-l"></div>
+                <div className="text-gray-400">Add guests</div>
+                <button className="bg-primary text-white p-1 rounded-full">
+                    <SearchIcon />
+                </button>
+            </div>
+            <Link
+                to={user ? "/profile" : "/login"}
+                className="flex items-center gap-3 border rounded-full py-2 px-4"
+            >
+                <HambargerIcon />
+                <div className="bg-gray-500 text-white rounded-full border-2 border-gray-500 overflow-hidden">
+                    <UserIcon />
+                </div>
+            </Link>
+        </header>
+    );
+};
+
+export default Header;
