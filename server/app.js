6b3da3b63f73f9ea788b9f83e32a094f8c72ae28
iamaiza <aiza9382@gmail.com>
2023-05-09 09:34:54 +0500

    register commit

--- server/app.js
+++ server/app.js
@@ -2,17 +2,20 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
+const userRoutes = require("./routes/userRoutes.js")
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+app.use(userRoutes)
 
 const URL = process.env.MONGO_CONNECTION_URL
 const port = process.env.PORT
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true)
 
 app.get("/test", (req, res) => {
     res.json({ message: "test" })
