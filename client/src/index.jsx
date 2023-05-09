c06526f0f9c3ae22add69985a6d01e5cbfda684b
iamaiza <aiza9382@gmail.com>
2023-05-09 15:29:11 +0500

    index.jsx updated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 export { default as Layout } from "./Layout"
 export { default as Login } from "./components/Login"
-export { default as Register } from "./components/Register"
+export { default as Register } from "./pages/Register"
 export { default as Home } from "./pages/Home"
 export { default as AccountPage } from "./pages/AccountPage"
