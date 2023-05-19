dcc23a1f5ce6d7662a8082de413b4e62c6a71959
iamaiza <aiza9382@gmail.com>
2023-05-19 14:12:10 +0500

    updated places added booking

--- client/src/components/AllImages.jsx
+++ client/src/components/AllImages.jsx
@@ -10,10 +10,11 @@ const AllImages = ({ place, setShowAllImages }) => {
     }
 
   return (
-    <div className='absolute inset-0 bg-white min-h-screen'>
-        <div className='px-8 pb-12 pt-28 bg-black text-white'>
-            <button className='block w-fit bg-transparent fixed left-8 top-8 ' onClick={closeImagesPopupHandler}>
+    <div className='absolute inset-0 bg-black min-h-screen'>
+        <div className='px-8 md:px-20 lg:px-32 pb-12 pt-20 bg-black text-white'>
+            <button className='block w-fit bg-white text-black py-1 px-8 rounded-2xl fixed left-8 top-8 flex items-center gap-1 text-xl' onClick={closeImagesPopupHandler}>
                 <CloseIcon />
+                Close
             </button>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {place?.images?.length > 0 && place.images.map((img, idx) => (
--- client/src/icons/PlaceIcons.jsx
+++ client/src/icons/PlaceIcons.jsx
@@ -217,12 +217,12 @@ const CloseIcon = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-10 h-10"
+            className="w-6 h-6"
         >
             <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                d="M6 18L18 6M6 6l12 12"
             />
         </svg>
     );
@@ -266,5 +266,5 @@ export {
     FilledStarIcon,
     ImageIcon,
     CloseIcon,
-    LocationIcon
+    LocationIcon,
 };
